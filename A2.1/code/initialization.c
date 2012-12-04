4830fccbe53638494cfa4d5d3048270e6ba97057
LRZ-SIM Funktionskennung <h039uan@lxa191.cos.lrz.de>
2012-12-04 10:42:26 +0100

    each process reads its own part of geometry

--- A2.1/code/initialization.c
+++ A2.1/code/initialization.c
@@ -96,147 +96,93 @@ int initialization(char* file_in, char* part_type, int* nintci, int* nintcf, int
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);    /// get number of processes
 
 
-    if (my_rank == 0) {
-        printf("Before reading the files %d\n", my_rank);
+    printf("Before reading the files %d\n", my_rank);
 
-        // read-in the input file
-        int f_status = read_binary_geo(file_in, &*nintci, &*nintcf, &*nextci, &*nextcf, &*lcc,
-                                       &glob_bs, &glob_be, &glob_bn, &glob_bw,
-                                       &glob_bl, &glob_bh, &glob_bp,
-                                       &*su, &*points_count,
-                                       &*points, elems);
+    // read-in the input file
+    int f_status = read_binary_geo(file_in, &*nintci, &*nintcf, &*nextci, &*nextcf, &*lcc,
+                                    &glob_bs, &glob_be, &glob_bn, &glob_bw,
+                                    &glob_bl, &glob_bh, &glob_bp,
+                                    &*su, &*points_count,
+                                    &*points, elems);
 
-        printf("Reading the files %d\n", my_rank);
+    printf("Reading the files %d\n", my_rank);
 
-        if ( f_status != 0 ) return f_status;
+    if ( f_status != 0 ) return f_status;
 
-        glob_var = (double*) calloc(sizeof(double), (*nextcf + 1));
-        glob_cgup = (double*) calloc(sizeof(double), (*nextcf + 1));
-        glob_oc = (double*) calloc(sizeof(double), (*nintcf + 1));
-        glob_cnorm = (double*) calloc(sizeof(double), (*nintcf + 1));
+    glob_var = (double*) calloc((*nextcf + 1), sizeof(double));
+    glob_cgup = (double*) calloc((*nextcf + 1), sizeof(double));
+    glob_oc = (double*) calloc((*nintcf + 1), sizeof(double));
+    glob_cnorm = (double*) calloc((*nintcf + 1), sizeof(double));
 
-        // initialize the arrays
-        for ( i = 0; i <= 10; i++ ) {
-            glob_oc[i] = 0.0;
-            glob_cnorm[i] = 1.0;
+    // initialize the arrays
+
+    for ( i = (*nintci); i <= (*nintcf); i++ )
+        (glob_cgup)[i] = 1.0 / (glob_bp[i]);
+
+
+    idx_t options[METIS_NOPTIONS];
+
+    ne = *nintcf+1;
+    nn = *points_count;
+
+    idx_t* eptr = (idx_t*) malloc((ne + 1) * sizeof(idx_t));
+    for ( i = 0; i < ne + 1; ++i )
+        eptr[i] = i * 8;
+
+
+    idx_t* eind = (idx_t*) malloc(ne * sizeof(idx_t) * 8);
+
+    for ( i = 0; i < ne * 8; ++i )
+        eind[i] = (*elems)[i];
+
+    ept = (idx_t*) malloc(ne * sizeof(idx_t));
+    npt = (idx_t*) malloc(nn * sizeof(idx_t));
+
+    idx_t obvl;
+    idx_t ncommon = 4;
+    idx_t procs = num_procs;
+
+    METIS_SetDefaultOptions(options);
+
+
+    if ( strcmp(part_type, "dual") == 0 ) {
+        if (METIS_OK != METIS_PartMeshDual(&ne, &nn,
+                                            eptr, eind,
+                                            NULL, NULL, &ncommon,
+                                            &procs, NULL, options,
+                                            &obvl, ept, npt))
+            printf("Error in METIS_PartMeshDual\n");
+    } else if ( strcmp(part_type, "nodal") == 0 ) {
+        METIS_PartMeshNodal(&ne, &nn,
+                            eptr, eind,
+                            NULL, NULL,
+                            &procs,
+                            NULL, options,
+                            &obvl, ept, npt);
+    } else if (strcmp(part_type, "classical") == 0) {
+        int elems_per_node = ne / procs + 1;
+        printf("Elements per node %d\n", elems_per_node);
+
+        for ( i = 0; i < ne; ++i ) {
+            int p = i / elems_per_node;
+            ept[i] = p;
         }
-
-        for ( i = (*nintci); i <= (*nintcf); i++ ) {
-            glob_cgup[i] = 0.0;
-            glob_var[i] = 0.0;
-        }
-
-        for ( i = (*nextci); i <= (*nextcf); i++ ) {
-            (glob_var)[i] = 0.0;
-            (glob_cgup)[i] = 0.0;
-            (glob_bs)[i] = 0.0;
-            (glob_be)[i] = 0.0;
-            (glob_bn)[i] = 0.0;
-            (glob_bw)[i] = 0.0;
-            (glob_bl)[i] = 0.0;
-            (glob_bh)[i] = 0.0;
-        }
-
-        for ( i = (*nintci); i <= (*nintcf); i++ )
-            (glob_cgup)[i] = 1.0 / (glob_bp[i]);
-
-
-        idx_t options[METIS_NOPTIONS];
-
-        ne = *nintcf+1;
-        nn = *points_count;
-
-        idx_t* eptr = (idx_t*) malloc((ne + 1) * sizeof(idx_t));
-        for ( i = 0; i < ne + 1; ++i )
-            eptr[i] = i * 8;
-
-
-        idx_t* eind = (idx_t*) malloc(ne * sizeof(idx_t) * 8);
-
-        for ( i = 0; i < ne * 8; ++i )
-            eind[i] = (*elems)[i];
-
-        ept = (idx_t*) malloc(ne * sizeof(idx_t));
-        npt = (idx_t*) malloc(nn * sizeof(idx_t));
-
-        idx_t obvl;
-        idx_t ncommon = 4;
-        idx_t procs = num_procs;
-
-        METIS_SetDefaultOptions(options);
-
-
-
-        if ( strcmp(part_type, "dual") == 0 ) {
-            if (METIS_OK != METIS_PartMeshDual(&ne, &nn,
-                                               eptr, eind,
-                                               NULL, NULL, &ncommon,
-                                               &procs, NULL, options,
-                                               &obvl, ept, npt))
-                printf("Error in METIS_PartMeshDual\n");
-        } else if ( strcmp(part_type, "nodal") == 0 ) {
-            METIS_PartMeshNodal(&ne, &nn,
-                                eptr, eind,
-                                NULL, NULL,
-                                &procs,
-                                NULL, options,
-                                &obvl, ept, npt);
-        } else if (strcmp(part_type, "classical") == 0) {
-            int elems_per_node = ne / procs + 1;
-            printf("Elements per node %d\n", elems_per_node);
-
-            for ( i = 0; i < ne; ++i ) {
-                int p = i / elems_per_node;
-                ept[i] = p;
-            }
-        } else {
-            printf("unknown partition type\n");
-            MPI_Abort(MPI_COMM_WORLD, -1);
-        }
-        free(eptr);
-        free(eind);
+    } else {
+        printf("unknown partition type\n");
+        MPI_Abort(MPI_COMM_WORLD, -1);
     }
 
-    // distribute lengths of METIS arrays
-    MPI_Bcast(&ne, 1, MPI_LONG, 0, MPI_COMM_WORLD);
-    MPI_Bcast(&nn, 1, MPI_LONG, 0, MPI_COMM_WORLD);
 
-    if ( my_rank != 0 ) {
-        ept = (idx_t*) malloc(ne * sizeof(idx_t));
-        npt = (idx_t*) malloc(nn * sizeof(idx_t));
-    }
-
-    // distribute METIS arrays
-    MPI_Bcast(ept, ne, MPI_LONG, 0, MPI_COMM_WORLD);
-    MPI_Bcast(npt, nn, MPI_LONG, 0, MPI_COMM_WORLD);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    int number_of_elements;
+    free(eptr);
+    free(eind);
 
     // filling local_global_index array
-    int* number_of_elements_arr = 0;
-    if (my_rank == 0) {
-        number_of_elements_arr = (int*) calloc(num_procs, sizeof(int));
-
-        // count number of elements for each process
-        for ( i = 0; i < ne; ++i ) {
-            number_of_elements_arr[ept[i]] += 1;
-        }
-
-        // Send number of elements to each process
-        for ( j = 1; j < num_procs; ++j ) {
-            MPI_Send(&(number_of_elements_arr[j]), 1, MPI_INT, j, 10, MPI_COMM_WORLD);
-        }
-
-        // explicit copyinf for 0 process
-        number_of_elements = number_of_elements_arr[0];
-    } else {
-        // each process gets its number of elements
-        MPI_Status status;
-        MPI_Recv(&number_of_elements, 1, MPI_INT, 0, 10, MPI_COMM_WORLD, &status);
+    int number_of_elements = 0;
+    // count number of elements for each process
+    for ( i = 0; i < ne; ++i ) {
+        if (ept[i] == my_rank)
+            number_of_elements += 1;
     }
-
 
     allocate_memory_for_distributed_arrays(local_global_index,
                                            cgup,
@@ -246,110 +192,37 @@ int initialization(char* file_in, char* part_type, int* nintci, int* nintcf, int
                                            bp,
                                            number_of_elements);
 
-    *global_local_index = malloc(ne, sizeof(int));
+    *global_local_index = (int*) calloc(ne, sizeof(int));
 
-    if ( my_rank == 0 ) {
-        for ( j = 1; j < num_procs; ++j ) {
-            int* local_global_index_send;
-            double* cgup_send;
-            double* bn_send;
-            double* be_send;
-            double* bs_send;
-            double* bw_send;
-            double* bh_send;
-            double* bl_send;
-            double* bp_send;
+    int current_index = 0;
+    for ( i = 0; i < ne; ++i ) {
+        if ( ept[i] == my_rank ) {
+            (*local_global_index)[current_index] = i;
+            (*global_local_index)[i] = current_index;
 
-            allocate_memory_for_distributed_arrays(&local_global_index_send,
-                                                   &cgup_send,
-                                                   &bn_send,
-                                                   &be_send,
-                                                   &bs_send,
-                                                   &bw_send,
-                                                   &bh_send,
-                                                   &bl_send,
-                                                   &bp_send,
-                                                   number_of_elements_arr[j]);
+            (*cgup)[current_index] = glob_cgup[i];
+            (*be)[current_index] = glob_be[i];
+            (*bs)[current_index] = glob_bs[i];
+            (*bw)[current_index] = glob_bw[i];
+            (*bh)[current_index] = glob_bh[i];
+            (*bl)[current_index] = glob_bl[i];
+            (*bp)[current_index] = glob_bp[i];
 
-            int current_index = 0;
-            for ( i = 0; i < ne; ++i ) {
-                if ( ept[i] == j ) {
-                    local_global_index_send[current_index] = i;
-                    (*global_local_index)[i] = current_index;
-
-                    cgup_send[current_index] = glob_cgup[i];
-                    be_send[current_index] = glob_be[i];
-                    bs_send[current_index] = glob_bs[i];
-                    bw_send[current_index] = glob_bw[i];
-                    bh_send[current_index] = glob_bh[i];
-                    bl_send[current_index] = glob_bl[i];
-                    bp_send[current_index] = glob_bp[i];
-
-                    ++current_index;
-                }
-            }
-
-
-            int ne_send = number_of_elements_arr[j];
-            if (j != 0) {
-                send_distributed_arrays(local_global_index_send,
-                                        cgup_send,
-                                        bn_send, be_send,
-                                        bs_send, bw_send,
-                                        bh_send, bl_send,
-                                        bp_send, ne_send, j);
-            } else {
-                // for 0 process we just copy values without MPI
-                memcpy(local_global_index, local_global_index_send, ne_send * sizeof(int));
-                memcpy(cgup, cgup_send, ne_send * sizeof(double));
-                memcpy(bn, bn_send, ne_send * sizeof(double));
-                memcpy(be, be_send, ne_send * sizeof(double));
-                memcpy(bs, bs_send, ne_send * sizeof(double));
-                memcpy(bw, bw_send, ne_send * sizeof(double));
-                memcpy(bh, bh_send, ne_send * sizeof(double));
-                memcpy(bl, bl_send, ne_send * sizeof(double));
-                memcpy(bp, bp_send, ne_send * sizeof(double));
-            }
-
-            free(local_global_index_send);
-            free(be_send);
-            free(bn_send);
-            free(bw_send);
-            free(bs_send);
-            free(bh_send);
-            free(bl_send);
-            free(bp_send);
+            ++current_index;
         }
-
-        free(glob_var);
-        free(glob_cgup);
-        free(glob_oc);
-        free(glob_cnorm);
-        free(glob_bs);
-        free(glob_be);
-        free(glob_bn);
-        free(glob_bw);
-        free(glob_bl);
-        free(glob_bh);
-        free(glob_bp);
     }
 
-    MPI_Bcast(*global_local_index, ne, MPI_INT, 0, MPI_COMM_WORLD);
-
-
-    // all processes receive distributed arrays
-    if (my_rank != 0) {
-        MPI_Status status;
-        MPI_Recv(*local_global_index, number_of_elements, MPI_INT, 0, 9, MPI_COMM_WORLD, &status);
-        MPI_Recv(*cgup, number_of_elements, MPI_DOUBLE, 0, 10, MPI_COMM_WORLD, &status);
-        MPI_Recv(*be, number_of_elements, MPI_DOUBLE, 0, 11, MPI_COMM_WORLD, &status);
-        MPI_Recv(*bn, number_of_elements, MPI_DOUBLE, 0, 12, MPI_COMM_WORLD, &status);
-        MPI_Recv(*bw, number_of_elements, MPI_DOUBLE, 0, 13, MPI_COMM_WORLD, &status);
-        MPI_Recv(*bs, number_of_elements, MPI_DOUBLE, 0, 14, MPI_COMM_WORLD, &status);
-        MPI_Recv(*bh, number_of_elements, MPI_DOUBLE, 0, 15, MPI_COMM_WORLD, &status);
-        MPI_Recv(*bl, number_of_elements, MPI_DOUBLE, 0, 16, MPI_COMM_WORLD, &status);
-        MPI_Recv(*bp, number_of_elements, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD, &status);
-    }
+    free(glob_var);
+    free(glob_cgup);
+    free(glob_oc);
+    free(glob_cnorm);
+    free(glob_bs);
+    free(glob_be);
+    free(glob_bn);
+    free(glob_bw);
+    free(glob_bl);
+    free(glob_bh);
+    free(glob_bp);
 
     *nintci = 0;
     *nintcf = number_of_elements -1;
