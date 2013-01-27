e6064b0b4cc5434be545528f424f1b2bc4b4f9dd
paceholder <paceholder@localhost.localdomain>
2013-01-27 21:17:49 +0100

    speedup with plots

--- A2.4/code/speedup.py
+++ A2.4/code/speedup.py
@@ -1,4 +1,5 @@
 
+from pylab import *
 
 files = ["cojack.geo.bin", "pent.geo.bin"]
 nums = [2,4,8,16,32,64]
@@ -7,13 +8,14 @@ dist = ["dual", "classical"]
 
 for d in dist:
     print "====================================="
-    for f in files:
+    clf()
+    for fn in files:
         print "====================================="
         inits = []
         computes = []
         finals = []
-        print f + "-" + d
-        if f.startswith("cojack"):
+        print fn + "-" + d
+        if fn.startswith("cojack"):
             inits = [0.27]
             computes = [20.67]
             finals = [22.23]
@@ -22,24 +24,41 @@ for d in dist:
             computes = [4.8]
             finals = [5.27]
         for n in nums:
-            filename = f + "-" + d + "-" + str(n) + ".txt"
+            filename = fn + "-" + d + "-" + str(n) + ".txt"
 
-            fi = open(filename, "r")
+            fi = open("./results/"+filename, "r")
             for l in fi.readlines():
                 if l.startswith("init"):
-                   s = l.split("=")
-                   inits.append(float(s[-1].strip()))
+                    s = l.split("=")
+                    inits.append(float(s[-1].strip()))
                 if l.startswith("compute"):
-                   s = l.split("=")
-                   computes.append(float(s[-1].strip()))
+                    s = l.split("=")
+                    computes.append(float(s[-1].strip()))
                 if l.startswith("final"):
-                   s = l.split("=")
-                   finals.append(float(s[-1].strip()))
+                    s = l.split("=")
+                    finals.append(float(s[-1].strip()))
 
-        for i in range(len(finals)):
-            finals[i] -= computes[i]
+        for i in range(len(inits)):
+            #finals[i] -= computes[i]
             computes[i] -= inits[i]
 
+        speedup = []
+        for i in range(1,len(inits)):
+            speedup.append( computes[0] / computes[i])
+
+
+        
+        if fn.startswith("coj"):
+            subplot(211)
+        else:
+            subplot(212)
+        plot(nums, speedup, 'or-', markersize=5)
+        grid(True)
+        xlabel('# procs')
+        ylabel('speedup')
+        xticks(nums)
+        subplots_adjust(hspace = 0.6)
+        title(fn)
 
         ll = lambda s: str(s)+"\t"
         pp = lambda x, y: x+ y
@@ -49,3 +68,5 @@ for d in dist:
         print reduce(pp, map(ll, computes))
         print reduce(pp, map(ll, finals))
 
+    savefig("../report/images/speedup"+d+".eps")
+#    show()
